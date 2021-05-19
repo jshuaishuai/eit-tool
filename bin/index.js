fddb8a655515b4001c698225f3947d929b4a84e1
jshuaishuai <ajiang115@qq.com>
2021-05-19 09:45:04 +0800

    init

---
+++ bin/index.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+// const {init} = require('eit-lib')
+// const fs = require('fs');
+// console.log(init(1));
+// console.log('eit-cli');
+
+// const argv = process.argv;
+
+// const command = argv[2];
+
+// console.log(command);
+
+// fs.readFile('')
+// console.log(argv);
+// const [option,param] = argv.slice(3);
+
+// console.log(option,param);
+
+console.log('eit-tool 欢迎使用');
